31278b7a1711fc905ccb4aa41fa26268d0c37217
Justas Palumickas <jpalumickas@gmail.com>
2020-07-20 19:04:38 +0300

    Rewrite to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import { useState, useEffect } from 'react';
 
-export default () => {
+const useWindowFocus = () => {
   const [focused, setFocused] = useState(document.hasFocus());
 
   useEffect(() => {
     const onFocus = () => {
       setFocused(true);
-    }
+    };
 
     const onBlur = () => {
       setFocused(false);
-    }
+    };
 
     window.addEventListener('focus', onFocus);
     window.addEventListener('blur', onBlur);
@@ -18,8 +18,10 @@ export default () => {
     return () => {
       window.removeEventListener('focus', onFocus);
       window.removeEventListener('blur', onBlur);
-    }
+    };
   }, []);
 
   return focused;
-}
+};
+
+export default useWindowFocus;
