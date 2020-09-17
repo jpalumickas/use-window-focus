58fbf474ca253a16314ee45d4d115366aeba8f9a
Justas Palumickas <jpalumickas@gmail.com>
2020-09-17 22:25:33 +0300

    Bump version to v1.2.0

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const useWindowFocus = () => {
   const [focused, setFocused] = useState(false);
 
   useEffect(() => {
-    // first render
+    // First render
     setFocused(document.hasFocus());
 
     const onFocus = () => setFocused(true);
